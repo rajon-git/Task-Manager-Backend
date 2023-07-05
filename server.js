ff8103726b4f6a6bb4d6b24109758480871b178e
rajon-git <rajon.zhsust15@gmail.com>
2023-07-05 21:54:42 +0600

    photo limit added

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors=require("cors");
 const helmet=require("helmet")
 const mongoose=require("mongoose")
 const morgan=require("morgan")
+const bodyParser = require('body-parser')
 require("dotenv").config();
 
 //middlewares implement
@@ -13,7 +14,10 @@ app.use(cors());
 app.use(helmet());
 app.use(express.json())
 app.use(morgan("dev"))
-app.use(express.urlencoded({extended:false}));
+//app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({limit:'50mb'}));
+app.use(express.json({limit:'50mb'}));
+app.use(bodyParser.json());
 //app.use(express.static('client/build'));
 
 // //routes implement
